ca918fdfcc7ada477b317833c19af63f5298f470
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-05-31 22:15:51 +1000

    this will add a dashboard with a list of all the users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function (req, res, next) {
+  const userId = req.session.passport.user;
+
+  // console.log('userId', userId);
+  // Do mongoose call to get User object
+  // Pass in user object into the render function
   res.render('index', { title: 'Express' });
+});
+
+router.get('/dashboard', ensureAuthenticated, function (req, res, next) {
+  const userId = req.session.passport.user;
+  User.find({})
+  .then(user => {
+    res.render('dashboard', { user: user });
+  });
+
 });
 
 function ensureAuthenticated(req, res, next) {
